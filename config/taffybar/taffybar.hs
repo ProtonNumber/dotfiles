29bcd105d876fcdff36be24b68a96d9d242c36ab
Will H <77733928+ProtonNumber@users.noreply.github.com>
2022-02-15 05:37:25 +0000

    initial commit

---
+++ config/taffybar/taffybar.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+import System.Taffybar
+import System.Taffybar.Information.CPU
+import System.Taffybar.SimpleConfig
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Text.CPUMonitor
+import System.Taffybar.Widget.Text.MemoryMonitor
+import System.Taffybar.Widget.Generic.Icon
+
+cpuCallback = do
+  (_, systemLoad, totalLoad) <- cpuLoad
+  return [ totalLoad, systemLoad ]
+
+myClockConfig = defaultClockConfig
+  { clockFormatString = " %R %d/%m/%y "
+  }
+  
+myWorkspaceConfig = defaultWorkspacesConfig
+  { maxIcons = Just 1
+  }
+
+main = do
+
+  let clock = textClockNewWith myClockConfig
+      clockIcon = iconImageWidgetNew ".config/taffybar/icons/clock.png"
+      cpu = textCpuMonitorNew " $total$% " 1
+      cpuIcon = iconImageWidgetNew ".config/taffybar/icons/cpu.png"
+      mem = textMemoryMonitorNew " $used$ " 1
+      memIcon = iconImageWidgetNew ".config/taffybar/icons/memory.png"
+      workspaces = workspacesNew myWorkspaceConfig
+      tray = sniTrayThatStartsWatcherEvenThoughThisIsABadWayToDoIt
+      simpleConfig = defaultSimpleTaffyConfig
+                       { startWidgets = [ workspaces ]
+                       , endWidgets = [ clock, clockIcon, cpu, cpuIcon, mem, memIcon, tray ]
+                       , barPosition = Top
+                       , barPadding = 10
+                       , barHeight = ScreenRatio $ 1 / 30
+                       , widgetSpacing = 0
+                       }
+  simpleTaffybar simpleConfig
